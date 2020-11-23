9e7eb2a700875392b24c97908e6d0b5cc5792e14
David Peklak <dpeklak@gmail.com>
2020-11-23 22:18:37 +0100

    add storage-bucket

---
+++ terraform/storage-bucket/main.tf
@@ -0,0 +1,32 @@
+provider "google" {
+  project = "mystic-fountain-258616"
+  region  = "europe-west6"
+  zone    = "europe-west6-a"
+  version = "~> 3.45"
+}
+
+data "terraform_remote_state" "service_account" {
+  backend = "local"
+
+  config = {
+    path = "${path.module}/../service-account/terraform.tfstate"
+  }
+}
+
+resource "google_storage_bucket" "artifact_store" {
+  name          = "peklak_artifact_store"
+  location      = "europe-west6"
+  storage_class = "REGIONAL"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+}
+
+
+resource "google_storage_bucket_iam_binding" "artifact_store_iam_binding" {
+  bucket = google_storage_bucket.artifact_store.name
+  role = "roles/storage.objectViewer"
+  members = [
+    "serviceAccount:${data.terraform_remote_state.service_account.outputs.host_service_account_email}",
+  ]
+}
